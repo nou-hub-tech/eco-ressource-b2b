f0418cd46a6f37b811f03138edab3964c04112fa
maram458 <maram.aroua@esprit.tn>
2026-04-17 11:45:45 +0100

    testing the ai product finder

--- src/app/components/inventory-scan/inventory-scan.ts
+++ src/app/components/inventory-scan/inventory-scan.ts
@@ -10,6 +10,9 @@ import { PdfGeneratorService } from '../../core/services/pdf-generator.service';
   styleUrls: ['./inventory-scan.css']
 })
 
+
+
+
 export class InventoryScanComponent implements OnInit, AfterViewInit {
   products: any[] = [];
   history: any[] = [];
