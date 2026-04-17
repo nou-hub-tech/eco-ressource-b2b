48109178d09baf106a1e240783506263763ca9a5
Slimi Linda <131091163+linda7899@users.noreply.github.com>
2026-04-17 20:28:26 +0000

    ajouter la recherche a la liste des delivery-order

--- src/app/features/transporter/delivery-order/components/delivery-order-list/delivery-order-list.component.ts
+++ src/app/features/transporter/delivery-order/components/delivery-order-list/delivery-order-list.component.ts
@@ -307,7 +307,7 @@ export class DeliveryOrderListComponent implements OnInit, OnDestroy {
         this.subscriptions.add(sub);
     }
 
-    // ================ STATISTIQUES ================
+    // ============= STATISTIQUES ==============
     
     loadStatistiques(): void {
         const sub = this.deliveryOrderService.getStatistiques().subscribe({
