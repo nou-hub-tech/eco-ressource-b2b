0222530e8f5f404d706448178af9a8a38fc8254d
ahmed <mansour.ahmed@esprit.tn>
2026-05-24 22:40:06 +0100

    reverted to local host

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
 
-  apiUrl: '/api',
-  productApiUrl: '/product',
+  apiUrl: 'http://localhost:9090/api',
+  productApiUrl: 'http://localhost:9090/product',
 
   /** URL de base pour les QR codes (tunnel ngrok, domaine, etc.). Vide = window.location.origin */
   qrCodeBaseUrl: 'https://coastline-rumor-falcon.ngrok-free.dev'
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
 
-  apiUrl: '/api',
+  apiUrl: 'http://localhost:9090/api',
 
   /** API Spring « gestion produit » (CRUD catalogue) */
-  productApiUrl: '/product',
+  productApiUrl: 'http://localhost:9090/product',
 
   /** URL de base pour les QR codes (tunnel ngrok, domaine, etc.). Vide = window.location.origin */
   qrCodeBaseUrl: ''
